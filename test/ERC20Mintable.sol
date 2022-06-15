18381d64a8b676b0082fe59cdc07843873522e1d
Ivan Kuznetsov <me@jeiwan.ru>
2022-06-15 19:51:10 +0700

    Deploy contracts in tests

--- test/ERC20Mintable.sol
+++ test/ERC20Mintable.sol
@@ -1,10 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.14;
 
-import "solmate/ERC20.sol";
+import "solmate/tokens/ERC20.sol";
 
 contract ERC20Mintable is ERC20 {
-  function mint() {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals
+    ) ERC20(_name, _symbol, _decimals) {}
 
-  }
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
 }
--- test/UniswapV3Pool.t.sol
+++ test/UniswapV3Pool.t.sol
@@ -2,13 +2,24 @@
 pragma solidity ^0.8.14;
 
 import "forge-std/Test.sol";
+import "./ERC20Mintable.sol";
 import "../src/UniswapV3Pool.sol";
 
 contract UniswapV3PoolTest is Test {
+    ERC20Mintable token0;
+    ERC20Mintable token1;
     UniswapV3Pool pool;
 
     function setUp() public {
-        pool = new UniswapV3Pool();
+        token0 = new ERC20Mintable("Ether", "ETH", 18);
+        token1 = new ERC20Mintable("USDC", "USDC", 18);
+
+        pool = new UniswapV3Pool(
+            address(token0),
+            address(token1),
+            14143684505937315, // 0.0002 ETH/USDC, 1.0001^(85174/2)
+            85174
+        );
     }
 
     function testExample() public {
