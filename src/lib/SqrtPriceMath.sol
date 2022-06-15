8d30598de67a23d89ae68c61601a938162a700e2
Ivan Kuznetsov <me@jeiwan.ru>
2022-06-15 16:02:38 +0700

    Simplify, not removing liquidity yet

--- src/lib/SqrtPriceMath.sol
+++ src/lib/SqrtPriceMath.sol
@@ -8,8 +8,7 @@ library SqrtPriceMath {
     function getAmount0Delta(
         uint160 sqrtPriceAX96,
         uint160 sqrtPriceBX96,
-        uint128 liquidity,
-        bool roundUp
+        uint128 liquidity
     ) internal pure returns (uint256) {
         if (sqrtPriceAX96 > sqrtPriceBX96)
             (sqrtPriceAX96, sqrtPriceBX96) = (sqrtPriceBX96, sqrtPriceAX96);
@@ -20,71 +19,25 @@ library SqrtPriceMath {
         require(sqrtPriceAX96 > 0);
 
         return
-            roundUp
-                ? Math.divRoundingUp(
-                    Math.mulDivRoundingUp(
-                        numerator1,
-                        numerator1,
-                        sqrtPriceBX96
-                    ),
-                    sqrtPriceAX96
-                )
-                : Math.mulDiv(numerator1, numerator2, sqrtPriceBX96) /
-                    sqrtPriceAX96;
+            Math.divRoundingUp(
+                Math.mulDivRoundingUp(numerator1, numerator2, sqrtPriceBX96),
+                sqrtPriceAX96
+            );
     }
 
     function getAmount1Delta(
         uint160 sqrtPriceAX96,
         uint160 sqrtPriceBX96,
-        uint128 liquidity,
-        bool roundUp
+        uint128 liquidity
     ) internal pure returns (uint256) {
         if (sqrtPriceAX96 > sqrtPriceBX96)
             (sqrtPriceAX96, sqrtPriceBX96) = (sqrtPriceBX96, sqrtPriceAX96);
 
         return
-            roundUp
-                ? Math.mulDivRoundingUp(
-                    liquidity,
-                    sqrtPriceBX96 - sqrtPriceAX96,
-                    FixedPoint96.Q96
-                )
-                : Math.mulDiv(
-                    liquidity,
-                    sqrtPriceBX96 - sqrtPriceAX96,
-                    FixedPoint96.Q96
-                );
-    }
-
-    function getAmount0Delta(
-        uint160 sqrtPriceAX96,
-        uint160 sqrtPriceBX96,
-        int128 liquidity
-    ) internal pure returns (uint256) {
-        return
-            liquidity < 0
-                ? 0 // TODO: implement removal of liquditiy
-                : getAmount0Delta(
-                    sqrtPriceAX96,
-                    sqrtPriceBX96,
-                    uint128(liquidity),
-                    true
-                );
-    }
-
-    function getAmount1Delta(
-        uint160 sqrtPriceAX96,
-        uint160 sqrtPriceBX96,
-        int128 liquidity
-    ) internal pure returns (uint256) {
-        return
-            liquidity < 0
-                ? 0 // TODO: implement removal of liquditiy
-                : getAmount0Delta(
-                    sqrtPriceAX96,
-                    sqrtPriceBX96,
-                    uint128(liquidity),
-                    true
-                );
+            Math.mulDivRoundingUp(
+                liquidity,
+                sqrtPriceBX96 - sqrtPriceAX96,
+                FixedPoint96.Q96
+            );
     }
 }
