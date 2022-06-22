aa89c7702f96addc185e83a9dd00712d20740059
Ivan Kuznetsov <me@jeiwan.ru>
2022-06-22 15:25:43 +0700

    Improve testMint

--- test/UniswapV3Pool.t.sol
+++ test/UniswapV3Pool.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.14;
 import "forge-std/Test.sol";
 import "./ERC20Mintable.sol";
 import "../src/UniswapV3Pool.sol";
+import "../src/lib/Position.sol";
 
 contract UniswapV3PoolTest is Test {
     ERC20Mintable token0;
@@ -19,22 +20,45 @@ contract UniswapV3PoolTest is Test {
         token0.mint(address(this), 1 ether);
         token1.mint(address(this), 5_000 ether);
 
+        int24 currentTick = 85176;
+        int24 lowerTick = 84222;
+        int24 upperTick = 86129;
+        uint128 liquidity = 1517882343751509868544;
+
         pool = new UniswapV3Pool(
             address(token0),
             address(token1),
             uint160(5602277097478614198912276234240), // current price, sqrt(5000) * 2**96
-            85176
+            currentTick
         );
 
         (uint256 amount0, uint256 amount1) = pool.mint(
             address(this),
-            84222,
-            86129,
-            1517882343751509868544
+            lowerTick,
+            upperTick,
+            liquidity
         );
 
-        assertEq(amount0, 0.998976618347425280 ether, "incorrect amount0");
-        assertEq(amount1, 5000 ether, "incorrect amount1");
+        uint256 expectedAmount0 = 0.998976618347425280 ether;
+        uint256 expectedAmount1 = 5000 ether;
+        assertEq(amount0, expectedAmount0, "incorrect amount0");
+        assertEq(amount1, expectedAmount1, "incorrect amount1");
+        assertEq(token0.balanceOf(address(pool)), expectedAmount0);
+        assertEq(token1.balanceOf(address(pool)), expectedAmount1);
+
+        bytes32 positionKey = keccak256(
+            abi.encodePacked(address(this), lowerTick, upperTick)
+        );
+        uint128 posLiquidity = pool.positions(positionKey);
+        assertEq(posLiquidity, liquidity);
+
+        (bool initialized, uint128 tickLiquidity) = pool.ticks(lowerTick);
+        assertTrue(initialized);
+        assertEq(tickLiquidity, liquidity);
+
+        (initialized, tickLiquidity) = pool.ticks(upperTick);
+        assertTrue(initialized);
+        assertEq(tickLiquidity, liquidity);
     }
 
     function uniswapV3MintCallback(uint256 amount0, uint256 amount1) public {
