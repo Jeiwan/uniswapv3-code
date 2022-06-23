57fdd6cf541db10d001e1c4e19fcbf5948794427
Ivan Kuznetsov <me@jeiwan.ru>
2022-06-23 16:00:04 +0700

    Extract and reuse test case params

--- test/UniswapV3Pool.t.sol
+++ test/UniswapV3Pool.t.sol
@@ -13,54 +13,85 @@ contract UniswapV3PoolTest is Test {
 
     bool shouldTransferInCallback = true;
 
+    struct TestCaseParams {
+        uint256 wethBalance;
+        uint256 usdcBalance;
+        int24 currentTick;
+        int24 lowerTick;
+        int24 upperTick;
+        uint128 liquidity;
+        uint160 currentSqrtP;
+    }
+
     function setUp() public {
         token0 = new ERC20Mintable("Ether", "ETH", 18);
         token1 = new ERC20Mintable("USDC", "USDC", 18);
     }
 
-    function testMint() public {
-        token0.mint(address(this), 1 ether);
-        token1.mint(address(this), 5_000 ether);
-
-        int24 currentTick = 85176;
-        int24 lowerTick = 84222;
-        int24 upperTick = 86129;
-        uint128 liquidity = 1517882343751509868544;
+    function setupTestCase(TestCaseParams memory params)
+        public
+        returns (uint256 poolBalance0, uint256 poolBalance1)
+    {
+        token0.mint(address(this), params.wethBalance);
+        token1.mint(address(this), params.usdcBalance);
 
         pool = new UniswapV3Pool(
             address(token0),
             address(token1),
-            uint160(5602277097478614198912276234240), // current price, sqrt(5000) * 2**96
-            currentTick
+            params.currentSqrtP,
+            params.currentTick
         );
 
-        (uint256 amount0, uint256 amount1) = pool.mint(
+        (poolBalance0, poolBalance1) = pool.mint(
             address(this),
-            lowerTick,
-            upperTick,
-            liquidity
+            params.lowerTick,
+            params.upperTick,
+            params.liquidity
         );
+    }
+
+    function testMint() public {
+        TestCaseParams memory params = TestCaseParams({
+            wethBalance: 1 ether,
+            usdcBalance: 5000 ether,
+            currentTick: 85176,
+            lowerTick: 84222,
+            upperTick: 86129,
+            liquidity: 1517882343751509868544,
+            currentSqrtP: 5602277097478614198912276234240
+        });
+        (uint256 poolBalance0, uint256 poolBalance1) = setupTestCase(params);
 
         uint256 expectedAmount0 = 0.998976618347425280 ether;
         uint256 expectedAmount1 = 5000 ether;
-        assertEq(amount0, expectedAmount0, "incorrect amount0");
-        assertEq(amount1, expectedAmount1, "incorrect amount1");
+        assertEq(
+            poolBalance0,
+            expectedAmount0,
+            "incorrect token0 deposited amount"
+        );
+        assertEq(
+            poolBalance1,
+            expectedAmount1,
+            "incorrect token1 deposited amount"
+        );
         assertEq(token0.balanceOf(address(pool)), expectedAmount0);
         assertEq(token1.balanceOf(address(pool)), expectedAmount1);
 
         bytes32 positionKey = keccak256(
-            abi.encodePacked(address(this), lowerTick, upperTick)
+            abi.encodePacked(address(this), params.lowerTick, params.upperTick)
         );
         uint128 posLiquidity = pool.positions(positionKey);
-        assertEq(posLiquidity, liquidity);
+        assertEq(posLiquidity, params.liquidity);
 
-        (bool initialized, uint128 tickLiquidity) = pool.ticks(lowerTick);
-        assertTrue(initialized);
-        assertEq(tickLiquidity, liquidity);
+        (bool tickInitialized, uint128 tickLiquidity) = pool.ticks(
+            params.lowerTick
+        );
+        assertTrue(tickInitialized);
+        assertEq(tickLiquidity, params.liquidity);
 
-        (initialized, tickLiquidity) = pool.ticks(upperTick);
-        assertTrue(initialized);
-        assertEq(tickLiquidity, liquidity);
+        (tickInitialized, tickLiquidity) = pool.ticks(params.upperTick);
+        assertTrue(tickInitialized);
+        assertEq(tickLiquidity, params.liquidity);
     }
 
     function testMintInvalidTickRangeLower() public {
