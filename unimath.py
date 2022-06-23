2071f8c5e361ce3e83121ec9e2f9d94b44115a79
Ivan Kuznetsov <me@jeiwan.ru>
2022-06-23 16:21:13 +0700

    Add unimath Python script

---
+++ unimath.py
@@ -0,0 +1,82 @@
+import math
+
+min_tick = -887272
+max_tick = 887272
+
+q96 = 2**96
+eth = 10**18
+
+
+def price_to_tick(p):
+    return math.floor(math.log(p, 1.0001))
+
+
+def price_to_sqrtp(p):
+    return int(math.sqrt(p) * q96)
+
+
+def tick_to_sqrtp(t):
+    return int((1.0001 ** (t / 2)) * q96)
+
+
+def liquidity0(amount, pa, pb):
+    if pa > pb:
+        pa, pb = pb, pa
+    return (amount * (pa * pb) / q96) / (pb - pa)
+
+
+def liquidity1(amount, pa, pb):
+    if pa > pb:
+        pa, pb = pb, pa
+    return amount * q96 / (pb - pa)
+
+
+def calc_amount0(liq, pa, pb):
+    if pa > pb:
+        pa, pb = pb, pa
+    return int(liq * q96 * (pb - pa) / pa / pb)
+
+
+def calc_amount1(liq, pa, pb):
+    if pa > pb:
+        pa, pb = pb, pa
+    return int(liq * (pb - pa) / q96)
+
+
+# Liquidity provision
+price_low = 4545
+price_cur = 5000
+price_upp = 5500
+
+print(f"Price range: {price_low}-{price_upp}; current price: {price_cur}")
+
+sqrtp_low = price_to_sqrtp(price_low)
+sqrtp_cur = price_to_sqrtp(price_cur)
+sqrtp_upp = price_to_sqrtp(price_upp)
+
+amount_eth = 1 * eth
+amount_usdc = 5000 * eth
+
+liq0 = liquidity0(amount_eth, sqrtp_cur, sqrtp_upp)
+liq1 = liquidity1(amount_usdc, sqrtp_cur, sqrtp_low)
+liq = int(min(liq0, liq1))
+
+print(f"Deposit: {amount_eth/eth} ETH, {amount_usdc/eth} USDC; liquidity: {liq}")
+
+# Swap USDC for ETH
+amount_in = 42 * eth
+
+print(f"Selling {amount_in/eth} USDC")
+
+price_diff = (amount_in * q96) // liq
+price_next = sqrtp_cur + price_diff
+
+print("New price:", (price_next / q96) ** 2)
+print("New sqrtP:", price_next)
+print("New tick:", price_to_tick((price_next / q96) ** 2))
+
+amount_in = calc_amount1(liq, price_next, sqrtp_cur)
+amountOut = calc_amount0(liq, price_next, sqrtp_cur)
+
+print("USDC in:", amount_in / eth)
+print("ETH out:", amountOut / eth)
