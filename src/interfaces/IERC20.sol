7e632ba26ba4ec186f2e32122cb44be333fbbc29
Ivan Kuznetsov <me@jeiwan.ru>
2022-06-23 15:50:23 +0700

    New interfaces

--- src/interfaces/IERC20.sol
+++ src/interfaces/IERC20.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.8.14;
 
 interface IERC20 {
     function balanceOf(address) external returns (uint256);
+    function transfer(address, uint256) external;
 }
---
+++ src/interfaces/IUniswapV3SwapCallback.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.14;
+
+interface IUniswapV3SwapCallback {
+    function uniswapV3SwapCallback(int256 amount0, int256 amount1) external;
+}
