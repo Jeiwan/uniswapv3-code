912e3106a3be9a57f18be65dc5c775895dc9ab89
Ivan Kuznetsov <me@jeiwan.ru>
2022-06-26 16:34:13 +0700

    Add transferFrom to IERC20

--- src/interfaces/IERC20.sol
+++ src/interfaces/IERC20.sol
@@ -2,6 +2,11 @@
 pragma solidity ^0.8.14;
 
 interface IERC20 {
+    function approve(address, uint256) external;
+
     function balanceOf(address) external returns (uint256);
+
     function transfer(address, uint256) external;
+
+    function transferFrom(address, address, uint256) external;
 }
