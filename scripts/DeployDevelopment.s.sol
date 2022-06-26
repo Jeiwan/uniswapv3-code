3a985b924bacde4b521c0992e267b52cb38b0d5c
Ivan Kuznetsov <me@jeiwan.ru>
2022-06-26 10:37:53 +0700

    Add deployment script

---
+++ scripts/DeployDevelopment.s.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.14;
+
+import "forge-std/console.sol";
+import "forge-std/Script.sol";
+import "../src/UniswapV3Pool.sol";
+import "../src/UniswapV3Manager.sol";
+import "../test/ERC20Mintable.sol";
+
+contract DeployDevelopment is Script {
+    ERC20Mintable token0;
+    ERC20Mintable token1;
+
+    function run() public {
+        uint256 wethBalance = 1 ether;
+        uint256 usdcBalance = 5000 ether;
+        int24 currentTick = 85176;
+        int24 lowerTick = 84222;
+        int24 upperTick = 86129;
+        uint128 liquidity = 1517882343751509868544;
+        uint160 currentSqrtP = 5602277097478614198912276234240;
+
+        vm.startBroadcast();
+        token0 = new ERC20Mintable("Ether", "ETH", 18);
+        token1 = new ERC20Mintable("USDC", "USDC", 18);
+
+        UniswapV3Pool pool = new UniswapV3Pool(
+            address(token0),
+            address(token1),
+            currentSqrtP,
+            currentTick
+        );
+
+        UniswapV3Manager manager = new UniswapV3Manager();
+
+        token0.mint(address(manager), wethBalance);
+        token1.mint(address(manager), usdcBalance);
+        manager.mint(address(pool), lowerTick, upperTick, liquidity);
+
+        vm.stopBroadcast();
+
+        console.log("WETH address", address(token0));
+        console.log("USDC address", address(token1));
+        console.log("Pool address", address(pool));
+        console.log("manager address", address(manager));
+    }
+}
