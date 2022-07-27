fee7d08c52d6d47a55ed4d0051875ed27145e8d6
Ivan Kuznetsov <me@jeiwan.ru>
2022-07-27 16:27:36 +0300

    Test failures of UniswapV3Factory

--- test/UniswapV3Factory.t.sol
+++ test/UniswapV3Factory.t.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.8.14;
 
 import "forge-std/Test.sol";
 import "./ERC20Mintable.sol";
+import "./TestUtils.sol";
 
 import "../src/interfaces/IUniswapV3Pool.sol";
 import "../src/UniswapV3Factory.sol";
 import "../src/UniswapV3Pool.sol";
 
-contract UniswapV3FactoryTest is Test {
+contract UniswapV3FactoryTest is Test, TestUtils {
     ERC20Mintable token0;
     ERC20Mintable token1;
     UniswapV3Factory factory;
@@ -48,5 +49,22 @@ contract UniswapV3FactoryTest is Test {
         (uint160 sqrtPriceX96, int24 tick) = pool.slot0();
         assertEq(sqrtPriceX96, 0, "invalid sqrtPriceX96");
         assertEq(tick, 0, "invalid tick");
+    }
+
+    function testCreatePoolIdenticalTokens() public {
+        vm.expectRevert(encodeError("TokensMustBeDifferent()"));
+        factory.createPool(address(token0), address(token0), 1);
+    }
+
+    function testCreateZeroTokenAddress() public {
+        vm.expectRevert(encodeError("TokenXCannotBeZero()"));
+        factory.createPool(address(token0), address(0), 1);
+    }
+
+    function testCreateAlreadyExists() public {
+        factory.createPool(address(token0), address(token1), 1);
+
+        vm.expectRevert(encodeError("PoolAlreadyExists()"));
+        factory.createPool(address(token0), address(token1), 1);
     }
 }
