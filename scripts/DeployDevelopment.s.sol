879864f8e2add3705a1fd271d01c19347105137e
Ivan Kuznetsov <me@jeiwan.ru>
2022-07-28 08:12:20 +0300

    Deploy the pool via Factory

--- scripts/DeployDevelopment.s.sol
+++ scripts/DeployDevelopment.s.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.14;
 import "forge-std/console.sol";
 import "forge-std/Script.sol";
 
-import "../src/UniswapV3Pool.sol";
+import "../src/UniswapV3Factory.sol";
 import "../src/UniswapV3Manager.sol";
+import "../src/UniswapV3Pool.sol";
 import "../src/UniswapV3Quoter.sol";
 import "../test/ERC20Mintable.sol";
 import "../test/TestUtils.sol";
@@ -14,19 +15,19 @@ contract DeployDevelopment is Script, TestUtils {
     function run() public {
         uint256 wethBalance = 10 ether;
         uint256 usdcBalance = 100000 ether;
-        int24 currentTick = tick(5000);
         uint160 currentSqrtP = sqrtP(5000);
 
+        // DEPLOYING
         vm.startBroadcast();
+
         ERC20Mintable token0 = new ERC20Mintable("Wrapped Ether", "WETH", 18);
         ERC20Mintable token1 = new ERC20Mintable("USD Coin", "USDC", 18);
 
-        UniswapV3Pool pool = new UniswapV3Pool(
-            address(token0),
-            address(token1),
-            currentSqrtP,
-            currentTick
+        UniswapV3Factory factory = new UniswapV3Factory();
+        UniswapV3Pool pool = UniswapV3Pool(
+            factory.createPool(address(token0), address(token1), 1)
         );
+        pool.initialize(currentSqrtP);
 
         UniswapV3Manager manager = new UniswapV3Manager();
         UniswapV3Quoter quoter = new UniswapV3Quoter();
@@ -35,9 +36,11 @@ contract DeployDevelopment is Script, TestUtils {
         token1.mint(msg.sender, usdcBalance);
 
         vm.stopBroadcast();
+        // DONE
 
         console.log("WETH address", address(token0));
         console.log("USDC address", address(token1));
+        console.log("Factory address", address(factory));
         console.log("Pool address", address(pool));
         console.log("Manager address", address(manager));
         console.log("Quoter address", address(quoter));
--- ui/src/config.js
+++ ui/src/config.js
@@ -1,9 +1,10 @@
 const config = {
   token0Address: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
   token1Address: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
-  poolAddress: '0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0',
-  managerAddress: '0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9',
-  quoterAddress: '0xdc64a140aa3e981100a9beca4e685f962f0cf6c9',
+  factoryAddress: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
+  poolAddress: '0x6D8610169BF2eb3D57C9aFE59Abb21906DA9FD2A',
+  managerAddress: '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707',
+  quoterAddress: '0x0165878A594ca255338adfa4d48449f69242Eb8F',
   ABIs: {
     'ERC20': require('./abi/ERC20.json'),
     'Pool': require('./abi/Pool.json'),
