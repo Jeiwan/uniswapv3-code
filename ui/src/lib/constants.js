6a24b127eb80a8960d774cf38f28a150d9437a48
Ivan Kuznetsov <me@jeiwan.ru>
2022-08-26 07:53:30 +0300

    Update Pair contract codehash

--- ui/src/lib/constants.js
+++ ui/src/lib/constants.js
@@ -8,6 +8,6 @@ const feeToSpacing = {
 }
 
 // forge inspect UniswapV3Pool bytecode| xargs cast keccak
-const poolCodeHash = "0x25e8f76b6ab9c18a8cdea3c355e7db914118000b3839068f21083ce884a4d24c";
+const poolCodeHash = "0x9dc805423bd1664a6a73b31955de538c338bac1f5c61beb8f4635be5032076a2";
 
 export { uint256Max, feeToSpacing, poolCodeHash };
