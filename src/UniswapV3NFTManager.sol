63ec29cedcff2d0b7a8805c3ec9131b8b1a4d85d
Ivan Kuznetsov <me@jeiwan.ru>
2022-08-28 20:20:12 +0300

    Implement NFT token minting

--- src/UniswapV3NFTManager.sol
+++ src/UniswapV3NFTManager.sol
@@ -1,15 +1,137 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.14;
 
-contract UniswapV3NFTManager {
+import "solmate/tokens/ERC721.sol";
+
+import "./interfaces/IERC20.sol";
+import "./interfaces/IUniswapV3Pool.sol";
+import "./lib/LiquidityMath.sol";
+import "./lib/PoolAddress.sol";
+import "./lib/TickMath.sol";
+
+contract UniswapV3NFTManager is ERC721 {
+    error NotAuthorized();
+    error SlippageCheckFailed(uint256 amount0, uint256 amount1);
+
     struct Position {
-        uint80 poolId; // token0 + token1 + fee
+        address pool;
         int24 lowerTick;
         int24 upperTick;
         uint128 liquidity;
     }
 
+    uint256 public totalSupply;
+
+    address public immutable factory;
+
     mapping(uint256 => Position) public positions;
 
-    constructor() {}
+    modifier isApprovedOrOwner(uint256 tokenId) {
+        address owner = ownerOf(tokenId);
+        if (
+            msg.sender != owner ||
+            !isApprovedForAll[owner][msg.sender] ||
+            getApproved[tokenId] != msg.sender
+        ) revert NotAuthorized();
+
+        _;
+    }
+
+    constructor(address factoryAddress)
+        ERC721("UnsiwapV3 NFT Positions", "UNIV3")
+    {
+        factory = factoryAddress;
+    }
+
+    struct MintParams {
+        address recipient;
+        address tokenA;
+        address tokenB;
+        uint24 fee;
+        int24 lowerTick;
+        int24 upperTick;
+        uint256 amount0Desired;
+        uint256 amount1Desired;
+        uint256 amount0Min;
+        uint256 amount1Min;
+    }
+
+    function mint(MintParams calldata params) public returns (uint256 tokenId) {
+        IUniswapV3Pool pool = getPool(params.tokenA, params.tokenB, params.fee);
+
+        (uint160 sqrtPriceX96, , , , ) = pool.slot0();
+        uint160 sqrtPriceLowerX96 = TickMath.getSqrtRatioAtTick(
+            params.lowerTick
+        );
+        uint160 sqrtPriceUpperX96 = TickMath.getSqrtRatioAtTick(
+            params.upperTick
+        );
+
+        uint128 liquidity = LiquidityMath.getLiquidityForAmounts(
+            sqrtPriceX96,
+            sqrtPriceLowerX96,
+            sqrtPriceUpperX96,
+            params.amount0Desired,
+            params.amount1Desired
+        );
+
+        (uint256 amount0, uint256 amount1) = pool.mint(
+            address(this),
+            params.lowerTick,
+            params.upperTick,
+            liquidity,
+            abi.encode(
+                IUniswapV3Pool.CallbackData({
+                    token0: pool.token0(),
+                    token1: pool.token1(),
+                    payer: msg.sender
+                })
+            )
+        );
+
+        if (amount0 < params.amount0Min || amount1 < params.amount1Min)
+            revert SlippageCheckFailed(amount0, amount1);
+
+        tokenId = totalSupply++;
+        _mint(params.recipient, tokenId);
+
+        positions[tokenId] = Position({
+            pool: address(pool),
+            lowerTick: params.lowerTick,
+            upperTick: params.upperTick,
+            liquidity: liquidity
+        });
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    //
+    // INTERNAL
+    //
+    ////////////////////////////////////////////////////////////////////////////
+    function getPool(
+        address token0,
+        address token1,
+        uint24 fee
+    ) internal view returns (IUniswapV3Pool pool) {
+        (token0, token1) = token0 < token1
+            ? (token0, token1)
+            : (token1, token0);
+        pool = IUniswapV3Pool(
+            PoolAddress.computeAddress(factory, token0, token1, fee)
+        );
+    }
+
+    function uniswapV3MintCallback(
+        uint256 amount0,
+        uint256 amount1,
+        bytes calldata data
+    ) public {
+        IUniswapV3Pool.CallbackData memory extra = abi.decode(
+            data,
+            (IUniswapV3Pool.CallbackData)
+        );
+
+        IERC20(extra.token0).transferFrom(extra.payer, msg.sender, amount0);
+        IERC20(extra.token1).transferFrom(extra.payer, msg.sender, amount1);
+    }
 }
