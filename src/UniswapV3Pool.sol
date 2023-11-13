bb277e79acdcd8c4a03a4dcf2f689fe48078ba32
Jeiwan <me@jeiwan.ru>
2023-11-13 19:32:09 +0700

    Don't update liquidity when next tick is not initialized

--- src/UniswapV3Pool.sol
+++ src/UniswapV3Pool.sol
@@ -222,7 +222,7 @@ contract UniswapV3Pool is IUniswapV3Pool {
 
             step.sqrtPriceStartX96 = state.sqrtPriceX96;
 
-            (step.nextTick, ) = tickBitmap.nextInitializedTickWithinOneWord(
+            (step.nextTick, step.initialized) = tickBitmap.nextInitializedTickWithinOneWord(
                 state.tick,
                 1,
                 zeroForOne
@@ -248,16 +248,18 @@ contract UniswapV3Pool is IUniswapV3Pool {
             state.amountCalculated += step.amountOut;
 
             if (state.sqrtPriceX96 == step.sqrtPriceNextX96) {
-                int128 liquidityDelta = ticks.cross(step.nextTick);
+                if (step.initialized) {
+                    int128 liquidityDelta = ticks.cross(step.nextTick);
 
-                if (zeroForOne) liquidityDelta = -liquidityDelta;
+                    if (zeroForOne) liquidityDelta = -liquidityDelta;
 
-                state.liquidity = LiquidityMath.addLiquidity(
-                    state.liquidity,
-                    liquidityDelta
-                );
+                    state.liquidity = LiquidityMath.addLiquidity(
+                        state.liquidity,
+                        liquidityDelta
+                    );
 
-                if (state.liquidity == 0) revert NotEnoughLiquidity();
+                    if (state.liquidity == 0) revert NotEnoughLiquidity();
+                }
 
                 state.tick = zeroForOne ? step.nextTick - 1 : step.nextTick;
             } else if (state.sqrtPriceX96 != step.sqrtPriceStartX96) {
